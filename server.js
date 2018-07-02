ded5b7744c452bd62dc8371eead719d5e22e39c4
thegoldgoat <thegoldgoat@gmail.com>
2018-07-02 17:36:37 +0200

    Added Encryption/Decryption.

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const crypto = require('crypto');
+const crypt_algorithm = 'aes-256-ctr';
+
+// Crypto functions
+function encrypt(buffer, password, algorithm) {
+  var cipher = crypto.createCipher(algorithm, password)
+  var crypted = Buffer.concat([cipher.update(buffer), cipher.final()]);
+  return crypted;
+}
+function decrypt(buffer, password, algorithm) {
+  var decipher = crypto.createDecipher(algorithm, password)
+  var dec = Buffer.concat([decipher.update(buffer), decipher.final()]);
+  return dec;
+}
 
 var app = express();
 // default options
 app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Main page
 app.get('/', function (req, res, next) {
-  var options = {
-    root: __dirname + '/doc/',
-    dotfiles: 'deny',
-    headers: {
-      'x-timestamp': Date.now(),
-      'x-sent': true
-    }
-  };
-  var fileName = 'index.html';
-  res.sendFile(fileName, options, function (err) {
-    if (!err)
-      console.log('Sent:', fileName);
-  });
+  var fileName = __dirname + '/doc/' + 'index.html';
+  res.sendFile(fileName);
 });
 
 // upload post method
@@ -30,14 +36,14 @@ app.post('/upload', function (req, res) {
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let sampleFile = req.files.sampleFile;
 
-  sampleFile.mv('/tmp/' + sampleFile.name, function (err) {
-    if (err)
-      return res.status(500).send(err);
-    console.log('File received!');
-  });
+  // Process the file buffer
+  var cryptFunction = req.body.isCrypt == 'True' ? encrypt : decrypt;
+  var newBuffer = cryptFunction(sampleFile.data, req.body.password, crypt_algorithm);
 
-  // Process the file
-
+  // Writes the buffer into file
+  var wstream = fs.createWriteStream('/tmp/' + sampleFile.name);
+  wstream.write(newBuffer);
+  wstream.end();
 
   res.send("<a href='/file/" + sampleFile.name + "'>Download</a>");
 });
