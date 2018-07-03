4a063da07b903215c12851bc3667e0ce197302e6
thegoldgoat <thegoldgoat@gmail.com>
2018-07-03 20:58:18 +0200

    Added signin and login routes. Missing database auth.

--- userdb/route.js
+++ userdb/route.js
@@ -4,10 +4,10 @@ const express = require('express');
 var router = express.Router();
 
 // Sign up sign in page
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   // Check if session is already active
   if (req.session.username) {
-    res.redirect('/myfiles')
+    res.redirect('/user/myfiles');
   }
   // If it is not logged, then display the login page
   var fileName = __dirname + '/doc/' + 'index.html';
@@ -15,9 +15,53 @@ router.get('/', function(req, res) {
 });
 
 // Files page
-router.get('/myfiles', function(req, res) {
+router.get('/myfiles', function (req, res) {
   // Check if session is already active
   res.send('files page!');
+});
+
+
+function check_username_pass(username, password) {
+  if (!username || !password ||
+    username.length == 0 || password.length == 0)
+    return false;
+  return true;
+}
+
+// Sign in
+router.post('/signin', function (req, res) {
+  // Get post informations
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (!check_username_pass(username, password))
+    res.sendStatus(400);
+
+  // Check if it is correct
+  if (true) {
+    req.session.username = username;
+    res.redirect('/user/myfiles');
+  }
+  req.session.username = undefined;
+  res.redirect('/user/');
+});
+
+// Log in
+router.post('/login', function (req, res) {
+  // Get post informations
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (!check_username_pass(username, password))
+    res.sendStatus(400);
+
+  // Check if it is correct
+  if (true) {
+    req.session.username = username;
+    res.redirect('/user/myfiles');
+  }
+  req.session.username = undefined;
+  res.redirect('/user/');
 });
 
 module.exports = router;
